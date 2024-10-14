a913c93dfce0f4e08b47df96f6553e6211aa3e6c
nazia-fatima <naziafatima27@gmail.com>
2024-10-14 12:25:36 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 010438494499.dkr.ecr.us-east-1.amazonaws.com
 
 # Docker pull image
-docker pull 010438494499.dkr.ecr.us-east-1.amazonaws.com/docker-project:$1
+docker pull 010438494499.dkr.ecr.us-east-1.amazonaws.com/sample_docker:$1
 
 #delete the existing container
 docker rm -f dotnet-app
 
 # dokcer run the image
-docker run -itd -p 8000:8000 --name dotnet-app 010438494499.dkr.ecr.us-east-1.amazonaws.com/docker-project:$1
+docker run -itd -p 8000:8000 --name dotnet-app 10438494499.dkr.ecr.us-east-1.amazonaws.com/sample_docker:$1
